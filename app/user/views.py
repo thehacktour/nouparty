3e7bb03e33ea079f7aba0212dda56f894c22b5d2
Atilio <atiliogoulart@gmail.com>
2021-07-03 12:30:44 -0300

    Setup user's post endpoint

--- app/user/views.py
+++ app/user/views.py
@@ -10,3 +10,20 @@ class UserEndPoint(APIView):
         user = UserModel.objects.all()
         serializer = UserSerializer(user,many=True)
         return Response(serializer.data)
+
+    def post(self, request):
+
+        data = {
+
+            'name' : request.data.get('name'),
+            'age' : request.data.get('age')
+
+        }
+
+        user_serializer = UserSerializer(data=data)
+
+        if user_serializer.is_valid():
+            user_serializer.save()
+            return Response(user_serializer.data)
+        else:
+            return Response(user_serializer.errors)
