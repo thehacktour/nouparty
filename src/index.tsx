4b68d734899a98ce3461347ae993b788a625538e
Atilio <atilio.jesus@grupoboticario.com.br>
2025-01-30 14:13:05 -0300

    chore: 🚧 remove all the not used files from project

---
+++ src/index.tsx
@@ -0,0 +1,9 @@
+import { Image, StyleSheet, Platform, View, Text } from 'react-native';
+
+export default function HomeScreen() {
+  return (
+    <View>
+      <Text>Me Contrata PF</Text>
+    </View>
+  );
+}
